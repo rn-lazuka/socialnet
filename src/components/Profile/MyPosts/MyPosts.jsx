1ea6bc7e1b6123919467f5543dd149a91d14da47
Roman Lazuka <rn.lazuka@gmail.com>
2019-12-09 00:02:14 +0300

    router connect

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,9 +6,15 @@ const MyPosts = () => {
     return (
         <div>
             My posts
+            <div>
+            <textarea> </textarea>
+                <button>Add post</button>
+            </div>
+            <div className={s.posts}>
             <Post/>
             <Post/>
             <Post/>
+            </div>
         </div>);
 }
 

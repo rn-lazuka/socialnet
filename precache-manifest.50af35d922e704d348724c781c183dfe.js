a3822a93fdb6efe1d59c288335d70226cc37b85a
Roman Lazuka <rn.lazuka@gmail.com>
2020-02-04 15:03:58 +0300

    Updates

---
+++ precache-manifest.50af35d922e704d348724c781c183dfe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f5c72a294bff869faee25de550104f",
+    "url": "/socialnet/index.html"
+  },
+  {
+    "revision": "cdc61a8f9b94f56437e3",
+    "url": "/socialnet/static/css/3.0a551963.chunk.css"
+  },
+  {
+    "revision": "488829843ee0acdc5b5e",
+    "url": "/socialnet/static/css/4.86807a88.chunk.css"
+  },
+  {
+    "revision": "39eb3a82162793d8db0d",
+    "url": "/socialnet/static/css/main.860e6143.chunk.css"
+  },
+  {
+    "revision": "0eb52a1a13d2315b9c39",
+    "url": "/socialnet/static/js/2.9b9cfd01.chunk.js"
+  },
+  {
+    "revision": "cdc61a8f9b94f56437e3",
+    "url": "/socialnet/static/js/3.0bf9c8d3.chunk.js"
+  },
+  {
+    "revision": "488829843ee0acdc5b5e",
+    "url": "/socialnet/static/js/4.be85b178.chunk.js"
+  },
+  {
+    "revision": "39eb3a82162793d8db0d",
+    "url": "/socialnet/static/js/main.d443f132.chunk.js"
+  },
+  {
+    "revision": "b448d86bcb1f0397b529",
+    "url": "/socialnet/static/js/runtime-main.86ccf8e0.js"
+  },
+  {
+    "revision": "dec3ae469dd213185ed580f4223f0396",
+    "url": "/socialnet/static/media/loading.dec3ae46.svg"
+  }
+]);
